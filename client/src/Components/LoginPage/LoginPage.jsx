7e2726869d83d8f01c54f43d80f56b200e3bd24f
joeinsanity <110877404+joeinsanity@users.noreply.github.com>
2024-10-17 21:03:02 -0500

    readme and loginpage updated
    readme and loginpage updated

--- client/src/Components/LoginPage/LoginPage.jsx
+++ client/src/Components/LoginPage/LoginPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; 
-import axios from 'axios';
 import './LoginPage.css';
 
 export const LoginPage = () => {
