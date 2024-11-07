95d9ee239c6256c099dfdce4e061f80a04e1e970
Svedek <28320440+Svedek@users.noreply.github.com>
2024-11-07 15:35:51 -0600

    like, unlike, and is post functions work

--- server/database/database.js
+++ server/database/database.js
@@ -24,20 +24,20 @@ async function getUser(username) {
 
 async function addLoginInfo(email, password) {
     const query = `INSERT INTO login_info (email, password) VALUES (?, ?)`;
-    const cols = await pool.query(query, [email, password]);
-    return cols[0].insertId;
+    const [rows] = await pool.query(query, [email, password]);
+    return rows.insertId;
 }
 
 async function addUser(username, major, loginID) {
     const query = `INSERT INTO user (username, major, user_login_info_id) VALUES (?, ?, ?)`;
-    const cols = await pool.query(query, [username, major, loginID]);
-    return cols[0].insertId;
+    const [rows] = await pool.query(query, [username, major, loginID]);
+    return rows.insertId;
 }
 
 async function addPost(owner, parent_post, text, is_event, time_posted) {
     const query = `INSERT INTO post (post_owner_user_id, post_parent_post_id, text, is_event, time_posted) VALUES (?, ?, ?, ?, ?)`;
-    const cols = await pool.query(query, [owner, parent_post, text, is_event, time_posted]);
-    return cols[0].insertId;
+    const [rows] = await pool.query(query, [owner, parent_post, text, is_event, time_posted]);
+    return rows.insertId;
 }
 
 async function getNextPosts(before, num_posts, filters) {
@@ -49,20 +49,26 @@ async function getNextPosts(before, num_posts, filters) {
         WHERE time_posted <= ?
         ORDER BY time_posted
         DESC LIMIT ?;`;
-    const cols = await pool.query(query, [before, num_posts]);
-    return cols;
+    const [rows] = await pool.query(query, [before, num_posts]);
+    return rows;
 }
 
 async function likePost(user_id, post_id) {
-    
+    const query = `INSERT INTO post_like (post_like_user_id, post_like_post_id) VALUES (?, ?)`;
+    const [rows] = await pool.query(query, [user_id, post_id]);
+    return rows.insertId;
 }
 
 async function unlikePost(user_id, post_id) {
-    
+    const query = `DELETE FROM post_like WHERE post_like_user_id=? AND post_like_post_id=?`;
+    const [rows] = await pool.query(query, [user_id, post_id]);
+    return rows.changedRows;
 }
 
 async function isPostLiked(user_id, post_id) {
-
+    const query = `SELECT * FROM post_like WHERE post_like_user_id=? AND post_like_post_id=?`;
+    const [rows] = await pool.query(query, [user_id, post_id]);
+    return rows.length > 0;
 }
 
 module.exports = { 
@@ -76,9 +82,9 @@ module.exports = {
 
 async function printAsync() {
     const date_old = new Date("2024-11-07T19:19:13.526Z");
-    console.log(date_old);
-    const date = new Date();
-    console.log(date);
+    // console.log(date_old);
+    // const date = new Date();
+    // console.log(date);
 
     // const login_info = await addLoginInfo("abcdefcvbasdf@uwm.edu", "pw23sdaf");
     // console.log(login_info);
@@ -89,7 +95,15 @@ async function printAsync() {
     // const res = await addPost(user, null, "asdg42f3", false, date);
     // console.log(res);
 
-    const posts = await getNextPosts(date_old, 5, null);
-    console.log(posts);
+    // const posts = await getNextPosts(date_old, 5, null);
+    // console.log(posts);
+
+    // const user_id = 13;
+    // const post_id = 7;
+    // console.log(await isPostLiked(user_id, post_id));
+    // console.log(await likePost(user_id, post_id));
+    // console.log(await isPostLiked(user_id, post_id));
+    // console.log(await unlikePost(user_id, post_id));
+    // console.log(await isPostLiked(user_id, post_id));
   }
 printAsync();
