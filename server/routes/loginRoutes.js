8d145b19baa17bb2216966cf7298b5b0e572eb50
brogers32 <111529735+brogers32@users.noreply.github.com>
2024-12-05 23:41:47 -0600

    current state of branch

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -8,10 +8,8 @@ const logoutRouter = express.Router();
 loginRouter.post("/", (req, res, next) => {
     passport.authenticate("local", async (err, login_info) => {
         if (login_info) {
-            console.log(login_info);
             let user = await getUserByLoginId(login_info.id);
             user = user[0];
-            console.log(user);
             req.logIn(user, () => {
                 return res.json({ auth: true });
             });
